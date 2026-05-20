2ec8f66ea61cdaf5185688a4f27d9cd2fb3c2ad5
ayumi <ay.kikuchi.sys25@morijyobi.ac.jp>
2026-05-20 14:48:19 +0900

    テストケース1~3の作成

--- Test/CalculatorTest.java
+++ Test/CalculatorTest.java
@@ -3,5 +3,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-    
+    @Test
+    void case1() {
+        Calculator calc = new Calculator();
+        String expected="EEEE";
+        String actual=calc.kic(-5);
+        assertEquals(expected,actual);
+    }
+    @Test
+    void case2() {
+        Calculator calc = new Calculator();
+        String expected="EEEE";
+        String actual=calc.kic(-1);
+        assertEquals(expected,actual);
+    }
+    @Test
+    void case3() {
+        Calculator calc = new Calculator();
+        String expected="0g";
+        String actual=calc.kic(0);
+        assertEquals(expected,actual);
+    }
     }
